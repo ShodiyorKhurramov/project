6f07bf56e46e9b98064dc9694593d7e226000c3d
ShodiyorXurramov <xurramoshodiyor377@gmail.com>
2022-11-03 18:16:07 +0500

    ss

---
+++ src/main/kotlin/minfin/uz/project/ProjectApplication.kt
@@ -0,0 +1,11 @@
+package minfin.uz.project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectApplication>(*args)
+}
