adfc5a9b4d74502d30d49a56cd9ab1b924efe484
ShodiyorXurramov <xurramoshodiyor377@gmail.com>
2022-11-21 16:36:10 +0500

    search

--- src/main/kotlin/uz/minfin/project/Controllers.kt
+++ src/main/kotlin/uz/minfin/project/Controllers.kt
@@ -1,11 +1,7 @@
 package uz.minfin.project
 
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
-import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 
